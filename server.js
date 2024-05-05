cd8116a21a8bc74dfe1a2f285c05ab810a1ff517
lindseymaddox9 <153481027+lindseymaddox9@users.noreply.github.com>
2024-05-04 21:16:45 -0500

    pulled index folder out due to error on render

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 const path = require("path");
 const notes = require("./db/db.json");
 const app = express();
